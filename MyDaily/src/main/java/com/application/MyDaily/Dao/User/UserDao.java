4cf8ea8bf41ea295ee9fecdf56239e5f2c642ced
gagrim1 <gaggy.rom@gmail.com>
2020-08-29 11:39:57 +0300

    From the beginning.

--- MyDaily/src/main/java/com/application/MyDaily/Dao/User/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.application.MyDaily.Dao.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.MyDaily.model.User;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
