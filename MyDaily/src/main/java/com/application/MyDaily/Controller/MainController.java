5564a7e8450611114d2e2d6e02d85492f7660a44
gagrim1 <gaggy.rom@gmail.com>
2020-09-01 16:53:23 +0300

    Now we can delete messages.

--- MyDaily/src/main/java/com/application/MyDaily/Controller/MainController.java
+++ MyDaily/src/main/java/com/application/MyDaily/Controller/MainController.java
@@ -2,12 +2,17 @@ package com.application.MyDaily.Controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.application.MyDaily.Repository.MessageDao;
 import com.application.MyDaily.domain.Message;
@@ -46,6 +51,16 @@ public class MainController {
 		}
 		
 		model.put("messages", messages);
+		return "Note";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id, Map<String, Object> model) {
+		mesDao.deleteById(id);
+		Iterable<Message> messages = mesDao.findAll();
+		model.put("messages", messages);
+		ModelAndView mav = new ModelAndView("Note");
+		mav.addObject("message", messages);
 		return "Note";
 	}
 }
