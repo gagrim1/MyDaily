7c33367b2d147cbd20aca0a963e6d650c7e6bda6
gagrim1 <gaggy.rom@gmail.com>
2020-09-05 16:44:46 +0300

    Small changes at Note.html and MainController

--- MyDaily/src/main/java/com/application/MyDaily/Controller/MainController.java
+++ MyDaily/src/main/java/com/application/MyDaily/Controller/MainController.java
@@ -1,14 +1,9 @@
 package com.application.MyDaily.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.application.MyDaily.Dao.MessageDao;
 import com.application.MyDaily.Services.MessageService;
 import com.application.MyDaily.domain.Message;
 
